0bc5a067522976161ccb09292aabb2d2c2ab4e76
MirgetaGashi <mirgetaagashi15@gmail.com>
2024-05-01 01:35:49 +0200

    Update

--- src/main/java/Database/Main.java
+++ src/main/java/Database/Main.java
@@ -5,10 +5,12 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Connection connection = DatabaseUtil.getConnection();
-        if(connection.isValid(1000)){
-            System.out.println("Lidhja me baze te te dhenave u krijuar me sukses!");
-        }
+
+            Connection connection = DatabaseUtil.getConnection();
+            if(connection.isValid(1000)){
+                System.out.println("Lidhja me baze te te dhenave u krijuar me sukses!");
+            }
+
     }
 
 }
