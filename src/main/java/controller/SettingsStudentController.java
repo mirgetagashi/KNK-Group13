c0c94a8d1f9bee083699f8a8a5ee130d650d50fd
Linda-Hasanaj <116763240+Linda-Hasanaj@users.noreply.github.com>
2024-05-13 13:47:36 +0200

    new stages

---
+++ src/main/java/controller/SettingsStudentController.java
@@ -0,0 +1,17 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class SettingsStudentController {
+    @FXML
+    private ChoiceBox<?> choiceBox;
+    @FXML
+    private TextField confirmNewPwd;
+
+    @FXML
+    private TextField newPwd;
+
+    @FXML
+    private TextField oldPwd;
+}
---
+++ src/main/java/controller/StudentProfileController.java
@@ -0,0 +1,48 @@
+package controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class StudentProfileController {
+    @FXML
+    private Label address;
+
+    @FXML
+    private Label birthday;
+
+    @FXML
+    private Button cancel;
+
+    @FXML
+    private Label email;
+
+    @FXML
+    private Label firstName;
+
+    @FXML
+    private Label gender;
+
+    @FXML
+    private Label lastName;
+
+    @FXML
+    private Label level;
+
+    @FXML
+    private Button modify;
+
+    @FXML
+    private Label school;
+
+    @FXML
+    void handleCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleModify(ActionEvent event) {
+
+    }
+
+}
