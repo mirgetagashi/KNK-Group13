f81d77d3af91376b28a37d3fda8f1145187ba413
Fjolla Kadriu <154750535+ffjolla@users.noreply.github.com>
2024-05-13 20:00:06 +0200

    Update Grades.java

--- src/main/java/model/Grades.java
+++ src/main/java/model/Grades.java
@@ -1,50 +1,57 @@
 package model;
 
+
 public class Grades {
     private int grade_id;
-    private Grade_level gradeLevel;
-    private Period period;
-    private Students student;
-    private Subject subject;
-    private Teacher teacher;
-    private int grade;
+    private int level_id;
+    private int period1_grade;
+    private int perios2_grade;
+    private int final_grade;
+    private int std_id;
+    private int subject_id;
+    private int t_id;
 
-    public Grades(int grade_id, Grade_level gradeLevel, Period period, Students student,
-                  Subject subject, Teacher teacher, int grade) {
+    public Grades(int grade_id, int level_id, int period1_grade, int perios2_grade, int final_grade, int std_id, int subject_id, int t_id) {
         this.grade_id = grade_id;
-        this.period = period;
-        this.student = student;
-        this.subject = subject;
-        this.teacher = teacher;
-        this.grade = grade;
-        this.gradeLevel=gradeLevel;
+        this.level_id = level_id;
+        this.period1_grade = period1_grade;
+        this.perios2_grade = perios2_grade;
+        this.final_grade = final_grade;
+        this.std_id = std_id;
+        this.subject_id = subject_id;
+        this.t_id = t_id;
     }
 
     public int getGrade_id() {
         return grade_id;
     }
 
-    public Period getPeriod() {
-        return period;
+    public int getLevel_id() {
+        return level_id;
     }
 
-    public Grade_level getGradeLevel() {
-        return gradeLevel;
+    public int getPeriod1_grade() {
+        return period1_grade;
     }
 
-    public Students getStudent() {
-        return student;
+    public int getPerios2_grade() {
+        return perios2_grade;
     }
 
-    public Subject getSubject() {
-        return subject;
+    public int getFinal_grade() {
+        return final_grade;
     }
 
-    public Teacher getTeacher() {
-        return teacher;
+    public int getStd_id() {
+        return std_id;
     }
 
-    public int getGrade() {
-        return grade;
+    public int getSubject_id() {
+        return subject_id;
+    }
+
+    public int getT_id() {
+        return t_id;
     }
 }
+
