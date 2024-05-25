b4335016b977ebdd7a4d14ec1f9e2f0c991ada1c
Linda-Hasanaj <116763240+Linda-Hasanaj@users.noreply.github.com>
2024-05-25 16:44:48 +0200

    ctrl shortcuts for student

--- src/main/java/controller/StudentNavigatorController.java
+++ src/main/java/controller/StudentNavigatorController.java
@@ -2,6 +2,8 @@ package controller;
 
 import app.Navigator;
 import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -25,6 +27,7 @@ public class StudentNavigatorController {
 
     @FXML
     void profile(MouseEvent event) {
+        Navigator.navigate(dashBo, Navigator.STUDENT_PROFILE);
 
     }
 
@@ -46,6 +49,24 @@ public class StudentNavigatorController {
     @FXML
     void student_statistics(MouseEvent event) {
         Navigator.navigate(dashBo, Navigator.STUDENT_TABLE);
+
+    }
+
+
+    @FXML
+    void handleKeyPressed(KeyEvent event) {
+        if (event.isControlDown() && event.getCode() == KeyCode.P) {
+
+            Navigator.navigate(root, Navigator.STUDENT_PROFILE);
+        } else if (event.isControlDown() && event.getCode() == KeyCode.D) {
+            Navigator.navigate(root,Navigator.STUDENT_CHART);
+
+        }else if (event.isControlDown() && event.getCode() == KeyCode.T) {
+            Navigator.navigate(root,Navigator.STUDENT_TABLE);
+        }else if (event.isControlDown() && event.getCode() == KeyCode.H) {
+            Navigator.navigate(root,Navigator.HELP_PAGE);
+
+        }
 
     }
 
